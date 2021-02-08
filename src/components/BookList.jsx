07d7cc80d3e097eccab28f15f5b44ea5d6036723
Pathum Liyanagama <pathumpml.dev@gmail.com>
2021-02-08 23:56:58 +0530

    Form to add a book

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,16 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 import BookDetail from './BookDetail';
 
 const BookList = () => {
-    const { books } = useContext(BookContext)
-    return (
-        <ul>
-            {books.map(book => (
-                <BookDetail key={book.id} book={book} />
-            ))}
-        </ul>
+    const { books, addBook } = useContext(BookContext)
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addBook(title, author);
+        setTitle("");
+        setAuthor("");
+    }
+
+    return (
+        <div>
+            <ul>
+                {books.map(book => (
+                    <BookDetail key={book.id} book={book} />
+                ))}
+            </ul>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder="Title" onChange={(e) => setTitle(e.target.value)} value={title} />
+                <input type="text" placeholder="Author" onChange={(e) => setAuthor(e.target.value)} value={author} />
+                <button type="submit">Add Book</button>
+            </form>
+        </div>
     );
 }
 
